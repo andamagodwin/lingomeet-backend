808198c57b7de00379857773b1cbf64f3c75e9cc
andamagodwin <andamagodwinezra@gmail.com>
2025-04-06 10:53:44 +0300

    deployment: changed env variables

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const oauth2Client = new OAuth2Client({
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirectUri: 'http://localhost:5000/api/auth/google/callback', // Must match EXACTLY
+  redirectUri: 'https://api.lingomeet.space/api/auth/google/callback', // Must match EXACTLY
 
 });
 
