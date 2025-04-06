5db2d5c8123673cb9a6dd6d1590bf2d75331f03b
andamagodwin <andamagodwinezra@gmail.com>
2025-04-06 09:55:16 +0300

    deployment: initial commit

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:3000', 'http://localhost:5173'], // Add both origins
     credentials: true,
-}));
+  }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
